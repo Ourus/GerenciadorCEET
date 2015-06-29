884c7b0e0b5a2ca06561c47d325825e5f720e889
Jonathan Dutra <jonatthan.dutra@gmail.com>
2015-06-29 10:19:30 -0300

    Classe professorBD : cadastro de professor

--- GerenciadorCeet/src/bandoDados/ProfessorBD.java
+++ GerenciadorCeet/src/bandoDados/ProfessorBD.java
@@ -1,7 +1,11 @@
 
 package bandoDados;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import modelos.Contato;
 import modelos.Desempenho;
 import modelos.Endereco;
@@ -10,24 +14,34 @@ import modelos.Professor;
 
 public class ProfessorBD extends FuncionarioBD
 {
-    public int cadastroProfessor(Professor professo, Endereco endereco, Contato contato, ArrayList<Escolaridade> escolaridade, ArrayList<Desempenho> desempenho)
+  
+    public int cadastroProfessor (Professor professor, Endereco endereco, Contato contato, ArrayList<Escolaridade> escolaridade, ArrayList<Desempenho> desempenho)
     {
-        int key=0;
-        
+        int key =0;
         try 
         {
-            key = super.cadastroFuncionario(professo, endereco, contato, escolaridade, desempenho);
-            
-            
+            key = super.cadastroFuncionario(professor, endereco, contato, escolaridade, desempenho);
+            conectarBanco();
+            String sql = "insert into professor(codProfessor,codDepartamento)values(?,?);";
+            PreparedStatement stm = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+               stm.setInt(1, key);
+            stm.setInt(2,professor.getCodDepartamento());
+            stm.executeUpdate();
+           
+            ResultSet codPessoa = stm.getGeneratedKeys();
+            while (codPessoa.next()) {
+                key = codPessoa.getInt(1);
+            }
+          
         } 
-        catch (Exception e) 
+        catch (Exception e)
         {
+            JOptionPane.showMessageDialog(null, e.getMessage());
             
         }
         finally
         {
             return key;
         }
-        
-    }
+    }    
 }
